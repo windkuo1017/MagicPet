f989b12866c5884fe43b1069b5b91618ab621a30
Usagi-19 <gobill35k@gmail.com>
2024-12-03 17:07:58 +0800

    Add files via upload

--- skill.cpp
+++ skill.cpp
@@ -5,22 +5,29 @@ using namespace std;
 
 int getRandomNum(int min, int max)
 {
-	return rand() % (max - min + 1) + min;
+	return rand() % (max - min + 1);
 }
 
+Skill::Skill(int _CD, string _name, int _hitRate)
+:formerCD(_CD),CD(0),name(_name),hitRate(_hitRate){}
+Skill::Skill(const Skill& another)
+:name(another.name),CD(another.CD),timesUsed(another.timesUsed),hitRate(another.hitRate),formerCD(another.formerCD){}
 
-  friend Skill::ostream& operator<<(ostream& os, const Skill& skill)
-  {
-      os << skill.name << "\n";
-      os << "招式威力：" << skill.power << "\n";
-      os << "當前CD回合：" << skill.CD << "\n";
-      os << "技能命中率：" << skill.hitRate << "%\n";
-      os << "已使用次數：" << skill.timesUsed << "\n";
-      return os;
-  }
+ostream& operator<<(ostream& os, const Skill& skill)
+{
+    os << skill.name << "\n";
+    os << "招式威力：" << skill.power << "\n";
+    os << "當前CD回合：" << skill.CD << "\n";
+    os << "技能命中率：" << skill.hitRate << "%\n";
+    os << "已使用次數：" << skill.timesUsed << "\n";
+    return os;
+}
 
-
-
+void Skill::reduceCD(){if(CD > 0) CD--;}
+void Skill::skillUsed(){CD = formerCD;}
+void Skill::printSkill(){cout << *this;}
+void Skill::printSkill(){cout << *this;}
+bool hitFail(){return getRandomNum(1,100) > hitRate;}
 bool Skill::useSkill(MagicPet* pokemon)
 {
     if(CD){
@@ -38,14 +45,18 @@ bool Skill::useSkill(MagicPet* pokemon)
 }
 
 
-  friend SpecialSkill::ostream& operator<<(ostream& os, const SpecialSkill& skill)
-  {
-      os << static_cast<const Skill&>(skill); 
-      os << "招式屬性：" << skill.type << "\n";
-      os << "使用次數限制：" << skill.timesLimit << "\n";
-      return os;
-  }
+ostream& operator<<(ostream& os, const SpecialSkill& skill)
+{
+    os << static_cast<const Skill&>(skill); 
+    os << "招式屬性：" << skill.type << "\n";
+    os << "使用次數限制：" << skill.timesLimit << "\n";
+    return os;
+}
 
+SpecialSkill::SpecialSkill(int _CD, string _name, int _hitRate, string _type, int _timesLimit)
+  :Skill(_CD, _name, _hitRate),type(_type),timesLimit(_timesLimit){}
+SpecialSkill::SpecialSkill(const SpecialSkill& another)
+  :Skill(another),type(another.type),timesLimit(another.timesLimit){}
 bool SpecialSkill::usedTooMuch()
 {
   if(timesUsed >= timesLimit)
--- skill.h
+++ skill.h
@@ -17,39 +17,31 @@ protected:
   int timesUsed = 0;//已使用次數
 
 public:
-  Skill(int _CD, string _name, int _hitRate):formerCD(_CD),CD(0),name(_name),hitRate(_hitRate){}
-  Skill(const Skill& another):name(another.name),CD(another.CD),timesUsed(another.timesUsed),hitRate(another.hitRate),formerCD(another.formerCD){} //copy constructor
+  Skill(int _CD, string _name, int _hitRate);
+  Skill(const Skill& another);
   virtual ~Skill() = default;
-  void skillUsed(){CD = formerCD;} //使用後技能CD恢復
-  void reduceCD(){if(CD > 0) CD--;}
-  void printSkill(){cout << *this;} //展示技能名稱
+  void skillUsed(); //使用後技能CD恢復
+  void reduceCD();
+  void printSkill(); //展示技能名稱
   virtual bool useSkill(MagicPet* pokemon); //使用技能
-  bool hitFail(){return getRandomNum(1,100) > hitRate;} //判定技能是否命中
-  friend ostream& operator<<(ostream& os, const Skill& skill)
-
+  bool hitFail(); //判定技能是否命中
+  friend ostream& operator<<(ostream& os, const Skill& skill);
 };
 
 
 class SpecialSkill : public Skill
 {
 friend class MagicPet; 
-  protected:
+protected:
   string type; //招式屬性
   int timesLimit; //使用次數限制
 
-  public:
-  SpecialSkill(int _CD, string _name, int _hitRate, string _type, int _timesLimit)
-  :Skill(_CD, _name, _hitRate),type(_type),timesLimit(_timesLimit){}
-  SpecialSkill(const SpecialSkill& another)
-  :Skill(another),type(another.type),timesLimit(another.timesLimit){} // copy constructor
+public:
+  SpecialSkill(int _CD, string _name, int _hitRate, string _type, int _timesLimit);
+  
+  SpecialSkill(const SpecialSkill& another); // copy constructor
   ~SpecialSkill() override;
   bool useSkill(MagicPet* pokemon) override;
   bool usedTooMuch();
-  friend ostream& operator<<(ostream& os, const SpecialSkill& skill)
-  {
-      os << static_cast<const Skill&>(skill); 
-      os << "招式屬性：" << skill.type << "\n";
-      os << "使用次數限制：" << skill.timesLimit << "\n";
-      return os;
-  }
-};
+  friend ostream& operator<<(ostream& os, const SpecialSkill& skill);
+ };
