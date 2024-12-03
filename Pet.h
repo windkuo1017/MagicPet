68efdd1247b19553c25e9213352456d70c4d4545
ShibaInu912 <167844675+ShibaInu912@users.noreply.github.com>
2024-12-04 00:28:49 +0800

    Modify overrid getExpAfterBattle

--- Pet.h
+++ Pet.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef MagicPet_H
 #define MagicPet_H
 
@@ -51,3 +52,58 @@ ostream& operator<<(ostream& out, const MagicPet& p); //輸出 MagicPet 的能力資訊
 
 #endif 
 
+=======
+#ifndef MagicPet_H
+#define MagicPet_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Skill; // Forward declaration
+
+class MagicPet 
+{
+protected:
+    string name;
+    string type; //屬性 
+    int Exp; //經驗值 
+    static const int EXP_LV = 10; //升等計算單位，非公式 
+    int level;
+    static const int MAX_LV = 100; //最大等級 
+   
+    int HP; //現在的 HP 
+    int maxHP;
+    int ATK; //attack
+    int DFS; //defense
+    int SPD; //speed
+
+
+
+	bool dodgeSuccessfully() const; //是否成功躲避 
+	bool dominate(MagicPet* opponent) const; //是否克制對手
+	
+public:
+	MagicPet(string name, string type, int HP, int s);
+	MagicPet(const MagicPet& other);
+	~MagicPet();
+ 
+	//getters
+	string getName() const { return name; }
+	string getType() const { return type; }
+	int getHP() const { return HP; } 
+	
+	vector<Skill*> skills;
+	
+	void initializeHP() { HP = maxHP; } //出場時回覆血量至最大值
+	bool attack(MagicPet* opponent, int skillID); //以第 skillID 招攻擊opponent 
+	void reduceSkillsCD(); //每回合結束時，減少 CD 時間 
+    virtual void getExpAfterBattle(bool win, int exp); //獲得經驗 
+    virtual void levelUp(); //升等可使能力提升 
+};
+
+ostream& operator<<(ostream& out, const MagicPet& p); //輸出 MagicPet 的能力資訊 
+
+#endif 
+
+>>>>>>> e52650bad2b5a7d275424c7ec98ae5c46c037c70
--- TypesOfPets.h
+++ TypesOfPets.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef TypesOfPet_H
 #define TypesOfPet_H
 
@@ -44,3 +45,51 @@ public:
 
 #endif
 
+=======
+#ifndef TypesOfPet_H
+#define TypesOfPet_H
+
+#include "Pet.h"
+
+// Gold Pet
+class GoldPet : public MagicPet {
+public:
+    GoldPet(string name, int HP, int speed);
+    void getExpAfterBattle(bool win) override;
+};
+
+// Water Pet
+class WaterPet : public MagicPet {
+public:
+    WaterPet(string name, int HP, int speed);
+    void levelUp() override;
+    void getExpAfterBattle(bool win) override;
+};
+
+// Fire Pet
+class FirePet : public MagicPet {
+public:
+    FirePet(string name, int HP, int speed);
+    void levelUp() override;
+    void getExpAfterBattle(bool win) override;
+};
+
+// Wood Pet
+class WoodPet : public MagicPet {
+public:
+    WoodPet(string name, int HP, int speed);
+    void levelUp() override;
+    void getExpAfterBattle(bool win) override;
+};
+
+// Mud Pet
+class MudPet : public MagicPet {
+public:
+    MudPet(string name, int HP, int speed);
+    void levelUp() override;
+    void getExpAfterBattle(bool win) override;
+};
+
+#endif
+
+>>>>>>> e52650bad2b5a7d275424c7ec98ae5c46c037c70
